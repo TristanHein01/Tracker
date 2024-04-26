6c3b419d16e5e50222487d45625bd9c57e962cd1
Tristan Hein <hein.tristan46@gmail.com>
2024-04-26 08:26:49 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 class Tracker {
     constructor() {
         this.previousCoords = null; // Vorherige Koordinaten
@@ -16,7 +17,7 @@ class Tracker {
             function successCallback(position) {
                 var speed = position.coords.speed;
                 var speedKmph = (speed * 3.6).toFixed(2);
-                document.getElementById("geschw").textContent = speedKmph;
+                document.getElementById("kmhslider").value = speedKmph;
                 console.log(speedKmph)
             }
 
@@ -71,7 +72,7 @@ class Tracker {
                 distance += calculateDistance(prevLat, prevLon, latitude, longitude);
             }
             this.previousCoords = { latitude, longitude };
-            const distanceElement = document.getElementById('distance');
+            const distanceElement = document.getElementById('valueshow');
             distanceElement.innerHTML = distance.toFixed(2); // Anzeige der Entfernung auf zwei Dezimalstellen
         };
 
@@ -90,5 +91,132 @@ class Tracker {
 }
 
 const tracker = new Tracker();
-tracker.getSpeed(); // Funktion zum Abrufen der Geschwindigkeit
-tracker.startStopTracker(); // Funktion zum Starten und Stoppen des GPS-Kilometerzählers
+
+tracker.getSpeed();
+tracker.startStopTracker(); 
+
+// kmh gauge script
+var basegradient = "repeating-conic-gradient(from 270deg, black  0deg 0.3deg, var(--color) 0.7deg 1.3deg, black  1.7deg 2deg)"
+
+//input values
+var total_kilometers = 172312;
+var totalkm = total_kilometers;
+var trip1_km = 352.7;
+var trip2_km = 124.1;
+var kmh = 0;
+kmh = Math.max(0, Math.min(kmh, 270));
+
+//log styles
+var green = "color: seagreen; background-color: black; border: 1px solid seagreen; padding: 4px;";
+var blue = "color: darkcyan; background-color: black; border: 1px solid darkcyan; padding: 4px;";
+var orange = "color: orange; background-color: black; border: 1px solid orange; padding: 4px;";
+var red = "color: red; background-color: black; border: 2px solid red; padding: 3px;";
+
+window.onload = function () {
+  console.clear();
+  console.log("%cInitialisation", green);
+};
+
+//initial sweep
+var sweepProgress = 0;
+var InitialSweepInterv = setInterval(initialSweep, 20);
+function initialSweep() {
+  if (sweepProgress < 54) {
+    kmh = kmh + 5;
+    sweepProgress++;
+    totalkm = 0;
+    km = 0;
+  }
+  else if (sweepProgress < 108) {
+    kmh = kmh - 5;
+    sweepProgress++;
+    totalkm = 999999;
+    km = 9999;
+    deci = 9;
+  }
+  else {
+    console.log("%cInitial Sweep Complete", green);
+    clearInterval(InitialSweepInterv);
+    //check if below is null to do things after initial sweep
+    InitialSweepInterv = null;
+    totalkm = total_kilometers;
+    trip = 2;
+    switchTrip();
+  }
+};
+
+//updating the gaug each frame
+var ScreenUpdateInterv = setInterval(screenUpdate, 20);
+function screenUpdate() {
+  //ensuring no half bars are displayed (1 bar = 2kmh)
+  if (kmh % 2 == 0) {
+    movescale();
+    updateOdometer();
+    updateTripmeter();
+  } else {
+    kmh--;
+    movescale();
+    updateOdometer();
+    updateTripmeter();
+    kmh++;
+  };
+};
+
+
+
+function movescale() {
+  document.getElementById("gauge").style = "background: " + basegradient + ", conic-gradient(from 270deg, white 0deg " + kmh + "deg, black " + kmh + "deg);"
+};
+
+function updateOdometer() {
+  document.getElementById("km_total").innerHTML = totalkm.toString().padStart(6, '0');
+}
+
+function updateTripmeter() {
+  document.getElementById("km_trip").innerHTML = km.toString().padStart(4, '0');
+  document.getElementById("km_trip_decimal").innerHTML = ". "+deci;
+
+}
+
+var km = 0;
+var deci = 0;
+var trip = 1;
+function switchTrip() {
+  switch (trip) {
+    case 1:
+      km = Math.floor(trip2_km);
+      deci = (trip2_km % 1).toFixed(1).substring(2);
+      trip = 2;
+      document.getElementById("tripswitch_2").style = "border: 1px solid var(--darkcolor); background-color: var(--color);"
+      document.getElementById("tripswitch_1").style = "border: 1px solid #022; background-color: #022;"
+      break;
+    case 2:
+      km = Math.floor(trip1_km);
+      deci = (trip1_km % 1).toFixed(1).substring(2);
+      trip = 1;
+      document.getElementById("tripswitch_1").style = "border: 1px solid var(--darkcolor); background-color: var(--color);"
+      document.getElementById("tripswitch_2").style = "border: 1px solid #022; background-color: #022;"
+      break;
+    default:
+      console.error("%ctrip switch state broken", red);
+      break;
+  }
+  console.log("%cswitching trip - "+trip+" - "+km+"."+deci, blue);
+};
+/*
+document.addEventListener('DOMContentLoaded', function () {
+  gauge.style = "background: " + basegradient + ", conic-gradient(from 270deg, white 0deg " + kmh + "deg, black " + kmh + "deg);"
+  slider.value = kmh;
+  output.innerHTML = slider.value;
+}, false);
+
+*/
+document.getElementById("kmhslider").oninput = function () {
+  if (InitialSweepInterv == null) {
+    kmh = this.value;
+  };
+  document.getElementById("valueshow").innerHTML = kmh;
+};
+
+
+
